939fb163e0e93dc8ff205e67ba52107b2d48bd87
Bruno <emerbruno@gmail.com>
2016-02-17 22:02:57 -0200

    Busca de arquivos e controle de versao

--- deploy-sqlqueries.sh
+++ deploy-sqlqueries.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+# Author: Bruno Emer
 
+FILE="version"
+SQL_DIR="_sql"
+
+VERSION=""
+if [ -f $FILE ]; then
+	VERSION=$(cat $FILE)
+fi
+
+for f in $(ls $SQL_DIR/*.sql); do
+	version_new=$(echo "$f" |cut -d"/" -f2 |cut -d"." -f1)
+	if [ "$version_new" -gt "$VERSION" ]; then
+		echo "$f"
+
+	fi
+done
+
+
+echo $version_new > $FILE
